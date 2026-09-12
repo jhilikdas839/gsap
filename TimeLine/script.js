fb119e2a3b4fe7698f23d7bc8529e820a29d5470
jhilikdas839 <jhilikdas839@gmail.com>
2026-09-12 21:10:08 +0530

    timeLine parameter learn

--- TimeLine/script.js
+++ TimeLine/script.js
@@ -4,22 +4,38 @@ const tl = gsap.timeline()
 
 tl.to(".box1",{
     x:1000,
-    duration:3,
+    duration:1.5,
     ease:"power3.out"
-}).to(".box2" ,{
+},"jhilik").to(".box2" ,{
     x:1000,
-    duration:3,
+    duration:1.5,
     ease:"power3.out"
-}).to(".box3" ,{
+},).to(".box3" ,{
     x:1000,
-    duration:3,
+    duration:1.5,
     ease:"power3.out"
-}).to(".box4" ,{
+},"jhilik").to(".box4" ,{
     x:1000,
-    duration:3,
+    duration:1.5,
     ease:"power3.out"
 }).to(".box5" ,{
     x:1000,
-    duration:3,
+    duration:1.5,
     ease:"power3.out"
-})
+},"jhilik")
+
+
+setTimeout(() => {
+
+    tl.reverse()
+}, 1000);
+
+
+
+//position parameter in time line 
+
+//-=0.3 ->this is used for overlapping effect 
+//+=0.3 ->this is used for delay effect 
+//0 ->this is used for start effect 
+//< ->this is used for parallal effect 
+//mylabel ->this is used for same  effect 
